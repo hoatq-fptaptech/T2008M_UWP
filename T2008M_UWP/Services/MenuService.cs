fc102794b71e1460e0c3d2875497cdeb656ac20e
hoatq-fptaptech <hoatq4@fpt.edu.vn>
2021-09-22 09:39:43 +0700

    adapter

--- T2008M_UWP/Services/MenuService.cs
+++ T2008M_UWP/Services/MenuService.cs
@@ -16,8 +16,9 @@ namespace T2008M_UWP.Services
         // task 2 --> làm task 2 kể cả task 1 chưa xong
         public async Task<Models.Categories> GetMenu()
         {
+            Adapters.FoodGroup api = Adapters.FoodGroup.GetInstance();
             HttpClient hc = new HttpClient(); // shipper -> lo việc kết nối api để lấy dữ liệu
-            var rs = await hc.GetAsync("http://foodgroup.herokuapp.com/api/menu");// get_file_content -> thao tác trả hàng của shipper
+            var rs = await hc.GetAsync(api.ApiMenu);// get_file_content -> thao tác trả hàng của shipper
             if (rs.StatusCode == HttpStatusCode.OK)
             {
                 var stringContent = await rs.Content.ReadAsStringAsync();// chuyển thành string json
