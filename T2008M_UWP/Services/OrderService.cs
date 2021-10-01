9b06f9fb7c86a48361852b50f9f893f1581c5d23
hoatq-fptaptech <hoatq4@fpt.edu.vn>
2021-10-01 09:27:48 +0700

    update

--- T2008M_UWP/Services/OrderService.cs
+++ T2008M_UWP/Services/OrderService.cs
@@ -13,20 +13,24 @@ namespace T2008M_UWP.Services
             CartService cs = new CartService();
             var items = cs.GetCart();
             // cach 2: items co the truyen trong tham so cuar function
-            Adapters.FoodGroup fg = Adapters.FoodGroup.GetInstance();
-            HttpClient httpClient = new HttpClient();
-            Uri uri = new Uri(fg.ApiCreateOrder);
-            HttpStringContent content = new HttpStringContent(
-                    "items:"+ JsonConvert.SerializeObject(items),
-                    UnicodeEncoding.Utf8,
-                    "application/json"
-                );
-            HttpResponseMessage msg = await httpClient.PostAsync(uri, content);
-            msg.EnsureSuccessStatusCode();
-            var rsBody = await msg.Content.ReadAsStringAsync();
-            Models.CreateOrder createOrder = JsonConvert.DeserializeObject<Models.CreateOrder>(rsBody);
-            return createOrder;
-            // sau khi nhan duoc order id -> luu vao 1 table trong SQLite de lam trang danh sach don hang
+            if(items.Count > 0)
+            {
+                Adapters.FoodGroup fg = Adapters.FoodGroup.GetInstance();
+                HttpClient httpClient = new HttpClient();
+                Uri uri = new Uri(fg.ApiCreateOrder);
+                HttpStringContent content = new HttpStringContent(
+                        "items:" + JsonConvert.SerializeObject(items),
+                        UnicodeEncoding.Utf8,
+                        "application/json"
+                    );
+                HttpResponseMessage msg = await httpClient.PostAsync(uri, content);
+                msg.EnsureSuccessStatusCode();
+                var rsBody = await msg.Content.ReadAsStringAsync();
+                Models.CreateOrder createOrder = JsonConvert.DeserializeObject<Models.CreateOrder>(rsBody);
+                return createOrder;
+                // sau khi nhan duoc order id -> luu vao 1 table trong SQLite de lam trang danh sach don hang
+            }
+            return null;
         }
 
     }
